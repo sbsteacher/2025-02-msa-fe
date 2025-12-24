688adbd287b4613346956f350d74ae6d9997aeb4
sbsteacher <id@gmail.com>
2025-12-24 15:39:49 +0900

    my_array

--- js/callback/cb04_mission02.js
+++ js/callback/cb04_mission02.js
@@ -8,11 +8,11 @@ const arr2 = arr.filter( item => !(item % 2) );
 console.log(arr2);
 const arr3 = arr2.map( item => item / 2 );
 console.log(arr3);
-arr3.forEach(item => console.log('item:', item));
+arr3.forEach( item => console.log('item:', item) );
 console.log('-----------------------');
 //체이닝 기법
 arr.filter( item => !(item % 2) )
     .map( item => item / 2 )
-    .forEach(item => console.log('item:', item))                
+    .forEach( item => console.log('item:', item) )
     ;
 // [ 44, 38, 17 ]
