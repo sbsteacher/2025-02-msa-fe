bd9d9e89dca3e4720597d124e6099d76b30190b7
sbsteacher <id@gmail.com>
2025-12-24 15:41:59 +0900

    ma_mission01(1)

---
+++ js/callback/ma_mission01.js
@@ -0,0 +1,16 @@
+/* ma_mission01.js  */
+const myArr = {
+    '0': 3,
+    '1': 88,
+    '2': 76,
+    '3': 9,
+    '4': 34,
+    '5': 65,
+    'length': 6,
+    'forEach': function() {        
+    }
+};
+
+myArr.forEach((item, idx) => {
+    console.log(`arr[${idx}]: ${item}`);
+});
